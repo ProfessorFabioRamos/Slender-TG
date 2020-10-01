00a6aac9cb22d40a2995946cd018e8b4b17d6f3e
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2020-10-01 07:54:24 -0300

    24-09-20

---
+++ Assets/Scripts/SlenderBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SlenderBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform playerTransform;
+    public SkinnedMeshRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(playerTransform.position);
+
+        if(rend.isVisible)
+            agent.speed = 0;
+        else
+            agent.speed = 2;
+
+        //agent.speed = rend.isVisible?agent.speed = 0:agent.speed = 2;
+    }
+}
